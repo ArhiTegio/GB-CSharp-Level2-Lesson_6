48ef6df45e52847c833e622620700e2d23daab0c
Arhio <Arhio@yandex.ru>
2019-10-17 09:17:50 +0300

    C# Уровень 2. Задание 6. Начало переработки.

---
+++ Company/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_CSharp_Level2_Lesson_5
+{
+    public class Department
+    {
+        string name = "";
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+        long profit = 0;
+
+        public Department(string name, long profit)
+        {
+            Name = name;
+            Profit = profit;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public long Profit { get => profit; set => profit = value; }
+        internal ObservableCollection<Employee> Employees { get => employees; set => employees = value; }
+
+        public override string ToString() => $" {Name} Прибыль: {Profit}";
+    }
+}
